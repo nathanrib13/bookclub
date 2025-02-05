4c16f193ba68db210b0892f55a688520b29169a6
Nathan Ribeiro <99979326+nathanrib13@users.noreply.github.com>
2025-02-05 20:50:34 -0300

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["m.media-amazon.com","upload.wikimedia.org", "br.web.img3.acsta.net"],
+  },
+};
+
+module.exports = nextConfig;
